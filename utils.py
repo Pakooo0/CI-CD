3f3b08ba4f098595e1fbbde48c9b27168912ac57
Michał P Stasiak <Michal.P.Stasiak@scanfil.com>
2024-05-08 14:17:34 +0200

    pull

---
+++ utils.py
@@ -0,0 +1,12 @@
+#przykładowe funkcje utils.py
+def add(a: int, b: int) -> int:
+    return a + b
+
+def substract(a: int, b: int) -> int:
+    return a - b
+
+def multiply(a: int, b: int) -> int:
+    return a * b
+
+def divide(a: int, b: int) -> int:
+    return a / b
